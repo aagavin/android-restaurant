917fdfc3ead9f0f22ce59dc73bce223f511903ed
Aaron <gitlab@aagavin.ca>
2017-09-21 12:15:04 -0400

    add new activity

---
+++ app/src/main/java/ca/aagavin/keanu/PizzaType.java
@@ -0,0 +1,13 @@
+package ca.aagavin.keanu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PizzaType extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pizza_type);
+    }
+}
