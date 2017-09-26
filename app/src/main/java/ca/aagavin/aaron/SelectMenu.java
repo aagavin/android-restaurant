e08b26a2b20a68dd9bfdcd84329dd511b5bd34a1
aferna95 <300773526@cencol.ca>
2017-09-26 16:03:56 -0400

    added scroll view to layout

--- app/src/main/java/ca/aagavin/aaron/SelectMenu.java
+++ app/src/main/java/ca/aagavin/aaron/SelectMenu.java
@@ -120,10 +120,10 @@ public class SelectMenu extends AppCompatActivity {
                 new MenuItem("Spring Rolls", 3.25, R.drawable.springrolls, "A lighter, more delicate version of egg rolls, made with a flour and water wrapper.", "ManChi's"),
                 new MenuItem("Deep Fried Wontons", 2.45, R.drawable.wonton, "Wonton wrappers filled with ground pork and a variety of vegetables and seasonings before deep-frying.", "ManChi's"),
                 new MenuItem("Chow Mein", 7.25, R.drawable.chowmein, "A stir-fried dish consisting of noodles, meat, onions and celery.", "HeyChina"),
-                new MenuItem("Egg Drop Soup", 7.05, R.drawable.eggDropSoup, "A classic dish -  flavored chicken broth or stock topped with silken threads of egg.", "HeyChina"),
+                new MenuItem("Egg Drop Soup", 7.05, R.drawable.eggdropsoup, "A classic dish -  flavored chicken broth or stock topped with silken threads of egg.", "HeyChina"),
                 new MenuItem("bangBangJi.jpgChicken and sweet corn soup", 6.05, R.drawable.chickensoup, "Chicken stock with creamed sweet corn, sweet corn kernels and eggs. The texture of this soup is often thick.", "Tim's Chinese Food"),
-                new MenuItem("Bang Bang Ji", 6.05, R.drawable.bangBangJi, "Chicken breasts are cut into matchstick sized pieces and served on a sheet of green bean paste. Made with hot chili oil.", "Tim's Chinese Food"),
-                new MenuItem("Lo Mein", 6.45, R.drawable.loMein, "Tossed Noodles - unlike chow mein, where the noodles are stir-fried separately, the noodles are tossed and blended with the stir-fry mixture.  They have more of a sauce than chow mein noodles.", "Pearl Harbourfront"),
+                new MenuItem("Bang Bang Ji", 6.05, R.drawable.bangbangji, "Chicken breasts are cut into matchstick sized pieces and served on a sheet of green bean paste. Made with hot chili oil.", "Tim's Chinese Food"),
+                new MenuItem("Lo Mein", 6.45, R.drawable.lomein, "Tossed Noodles - unlike chow mein, where the noodles are stir-fried separately, the noodles are tossed and blended with the stir-fry mixture.  They have more of a sauce than chow mein noodles.", "Pearl Harbourfront"),
                 new MenuItem("General Tsao's Chicken", 9.95, R.drawable.generaltso, "Chicken cubes coated in cornstarch and deep-fried, cooked with a sauce that includes hoisin sauce, dark soy sauce and chili peppers.", "Pearl Harbourfront")
 
         )));
@@ -143,25 +143,29 @@ public class SelectMenu extends AppCompatActivity {
                 new MenuItem("Cheeseburger", 10.01, R.drawable.cheeseburger, "Hamburger accompanied with melted cheese. The cheese is usually sliced, then added a short time before the hamburger finishes cooking to allow it to melt.", "Museum Tavern"),
                 new MenuItem("Australasian Burgers", 15.14, R.drawable.newzealand, "Popular regional hamburger in Australia and New Zealand includes canned beetroot and pineapple", "Museum Tavern"),
                 new MenuItem("Angus burger", 12.14, R.drawable.angus, "A hamburger made using beef from Angus cattle", "Woody's Burgers"),
-                new MenuItem("Bøfsandwich", 15.14, R.drawable.danishSandwich, "Classic Danish take on a hamburger. It contains the hamburger elements of a cooked ground beef patty placed inside a sliced bread roll", "Woody's Burgers"),
-                new MenuItem("Butter burger", 15.14, R.drawable.butterDouble, "Made with a buttered bun, butter as one of the ingredients of the patty or with a pat of butter on top of the burger patty.", "Five Guys"),
-                new MenuItem("Sloppy joes", 15.14, R.drawable.sloppyJoe, "A sandwich consisting of ground beef, onions, tomato sauce or ketchup, Worcestershire sauce, and other seasonings, served on a hamburger bun.", "Five Guys")
+                new MenuItem("Bøfsandwich", 15.14, R.drawable.danishsandwich, "Classic Danish take on a hamburger. It contains the hamburger elements of a cooked ground beef patty placed inside a sliced bread roll", "Woody's Burgers"),
+                new MenuItem("Butter burger", 15.14, R.drawable.butterdouble, "Made with a buttered bun, butter as one of the ingredients of the patty or with a pat of butter on top of the burger patty.", "Five Guys"),
+                new MenuItem("Sloppy joes", 15.14, R.drawable.sloppyjoe, "A sandwich consisting of ground beef, onions, tomato sauce or ketchup, Worcestershire sauce, and other seasonings, served on a hamburger bun.", "Five Guys")
         )));
         this._restaurant.put("Pizza", new ArrayList<>(Arrays.asList(
-                new MenuItem("Neapolitan pizza", 21.13, R.drawable.neapolitanpizza, "Made with San Marzano tomatoes and mozzarella cheese"),
-                new MenuItem("Hawaiian pizza", 21.10, R.drawable.pineapplepizza, "Topped with tomato sauce, cheese, pineapple,Canadian bacon and ham"),
-                new MenuItem("Meatball pizza", 23.45, R.drawable.panmeatball, "Prepared with meatballs. Increasingly popular in Upstate New York in contemporary times"),
-                new MenuItem("Chicago-style deep-dish pizza", 28.11, R.drawable.deepdish, "High edge and a deep surface for large amounts of cheese and chunky tomato sauce topped with pepperoni"),
-                new MenuItem("Sicilian Pizza", 28.11, R.drawable.sicilian, "square cut, thick crust pizza from, it should always have a spongier consistency than other pizzas."),
-                new MenuItem("Greek Pizza", 28.11, R.drawable.greekPizza, "baked in a round pan that has been heavily coated in olive oil. Lining the pan with oil also allows the bottom of the dough to fry while it bakes."),
-                new MenuItem("Chicago-style deep-dish pizza", 28.11, R.drawable.deepdish, "High edge and a deep surface for large amounts of cheese and chunky tomato sauce topped with pepperoni"),
-                new MenuItem("Chicago-style deep-dish pizza", 28.11, R.drawable.deepdish, "High edge and a deep surface for large amounts of cheese and chunky tomato sauce topped with pepperoni")
+                new MenuItem("Neapolitan pizza", 21.13, R.drawable.neapolitanpizza, "Made with San Marzano tomatoes and mozzarella cheese", "Domino's Pizza"),
+                new MenuItem("Hawaiian pizza", 21.10, R.drawable.pineapplepizza, "Topped with tomato sauce, cheese, pineapple,Canadian bacon and ham", "Domino's Pizza"),
+                new MenuItem("Meatball pizza", 23.45, R.drawable.panmeatball, "Prepared with meatballs. Increasingly popular in Upstate New York in contemporary times", "Papa John's Pizza"),
+                new MenuItem("Chicago-style deep-dish pizza", 28.11, R.drawable.deepdish, "High edge and a deep surface for large amounts of cheese and chunky tomato sauce topped with pepperoni", "Magic Oven"),
+                new MenuItem("Sicilian Pizza", 28.11, R.drawable.sicilian, "square cut, thick crust pizza from, it should always have a spongier consistency than other pizzas.", "Magic Oven"),
+                new MenuItem("Greek Pizza", 28.11, R.drawable.greekpizza, "baked in a round pan that has been heavily coated in olive oil. Lining the pan with oil also allows the bottom of the dough to fry while it bakes.", "Boston Pizza"),
+                new MenuItem("California Pizza", 28.11, R.drawable.calpizza, "Single-serving pizza that combines New York and Italian thin crust with toppings from the California cuisine", "Boston Pizza"),
+                new MenuItem("Pizza Cone", 28.11, R.drawable.pizzacone, "The latest and greatest way to eat pizza", "Papa John's Pizza")
         )));
         this._restaurant.put("Greek", new ArrayList<>(Arrays.asList(
-                new MenuItem("Lagana Bread", 6.34, R.drawable.lagana, "Traditional greek flatbread topped with sesame seeds"),
-                new MenuItem("Greek salad", 5.33, R.drawable.greeksalad,"Made with pieces of tomatoes, sliced cucumbers, onion, feta cheese"),
-                new MenuItem("Gyro",9.45, R.drawable.gyro,"Made outside with pork, or chicken, and cooked on a vertical rotisserie."),
-                new MenuItem("Baklava", 5.25, R.drawable.baklava, "A rich, sweet dessert pastry made of layers of filo filled with chopped nuts and sweetened with honey.")
+                new MenuItem("Lagana Bread", 6.34, R.drawable.lagana, "Traditional greek flatbread topped with sesame seeds", "Louis Souvlaki & Grill"),
+                new MenuItem("Greek salad", 5.33, R.drawable.greeksalad,"Made with pieces of tomatoes, sliced cucumbers, onion, feta cheese", "Louis Souvlaki & Grill"),
+                new MenuItem("Gyro",9.45, R.drawable.gyro,"Made outside with pork, or chicken, and cooked on a vertical rotisserie.", "The Arkadia House"),
+                new MenuItem("Baklava", 5.25, R.drawable.baklava, "A rich, sweet dessert pastry made of layers of filo filled with chopped nuts and sweetened with honey.", "Jimmy The Greek"),
+                new MenuItem("Calamari",9.45, R.drawable.calamari,"Garnished with lemon and parsley and served with some sort of dipping sauce.", "Jimmy The Greek"),
+                new MenuItem("Souvlaki",9.45, R.drawable.souvlaki,"grilled small pieces of meat served on the skewer for eating out of hand.", "The Arkadia House"),
+                new MenuItem("Orzo with Shrimp ",9.45, R.drawable.orzo,"risotto cooking of rice for an easy, comforting main dish", "The Souvlaki Hut"),
+                new MenuItem("Greek Yogurt",9.45, R.drawable.yaourt,"Yogurt with honey and walnuts.", "The Souvlaki Hut")
         )));
 
 
