e84d407a7dd02d9866fd41f1a500e1348054977c
aagavin <aagavin@gmail.com>
2017-09-27 19:29:04 -0400

    validation Order activity

--- app/src/main/java/ca/aagavin/aaron/CustomerInformation.java
+++ app/src/main/java/ca/aagavin/aaron/CustomerInformation.java
@@ -1,12 +1,23 @@
 package ca.aagavin.aaron;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.regex.Pattern;
+
 public class CustomerInformation extends AppCompatActivity {
+
+    public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile(
+                    "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",
+                    Pattern.CASE_INSENSITIVE
+            );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,17 +27,77 @@ public class CustomerInformation extends AppCompatActivity {
 
     public void button_confirm_click(View view){
         boolean allInputsValid;
-        allInputsValid = this._nameTextValid();
+        allInputsValid = this._emptyTextValid(R.id.editText_name);
+        allInputsValid = this._emptyTextValid(R.id.editText_address);
 
-        Toast.makeText(this, "Valid", Toast.LENGTH_SHORT).show();
+        allInputsValid = this._emptyTextValid(R.id.editText_cc);
+        allInputsValid = this._ccTextValid(R.id.editText_cc);
 
+        allInputsValid = this._emptyTextValid(R.id.editText_email);
+        allInputsValid = this._emailTextValid(R.id.editText_email);
+
+
+        allInputsValid = this._emptyTextValid(R.id.editText_moreInfo);
+
+        allInputsValid = this._emptyTextValid(R.id.editText_phone);
+        allInputsValid = this._phoneValid(R.id.editText_phone);
+
+
+        if(!allInputsValid) {
+            Toast.makeText(this, "Not all inputs are valid", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Intent i = new Intent(this, OrderActivity.class);
+
+            startActivity(i);
+        }
     }
 
-    private boolean _nameTextValid(){
-        TextView name = (TextView) findViewById(R.id.editText_name);
-        boolean valid = name.getText().toString().length()!=0;
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getTitle() == null){
+            AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+            alertDialog.setTitle("Nothing 222");
+            alertDialog.show();
+            return true;
+        }
+        else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private boolean _emptyTextValid(int id){
+        TextView textView = (TextView) findViewById(id);
+        boolean valid = textView.getText().toString().length()!=0;
         if (!valid){
-            name.setError("Name can't be empty");
+            textView.setError("Value Can't be empty");
+        }
+        return valid;
+    }
+
+    private boolean _ccTextValid(int id){
+        TextView textView = (TextView) findViewById(id);
+        boolean valid = textView.getText().toString().length()==16;
+        if (!valid){
+            textView.setError("Value needs to be 16 digits");
+        }
+        return valid;
+    }
+
+    private  boolean _phoneValid(int id){
+        TextView textView = (TextView) findViewById(id);
+        boolean valid = textView.getText().toString().matches("[0-9]+") && textView.getText().length() == 10;
+        if (!valid){
+            textView.setError("Phone number needs to be 10 digits");
+        }
+        return valid;
+    }
+
+    private boolean _emailTextValid(int id){
+        TextView textView = (TextView) findViewById(id);
+        boolean valid = VALID_EMAIL_ADDRESS_REGEX.matcher(textView.getText().toString()).find();
+        if (!valid){
+            textView.setError("Email address not valid");
         }
         return valid;
     }
