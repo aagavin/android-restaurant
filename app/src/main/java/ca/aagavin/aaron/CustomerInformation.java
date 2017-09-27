851ce4733a26d7d164b2e6782ab1652d2e0b349c
aagavin <aagavin@gmail.com>
2017-09-27 17:09:17 -0400

    validation

--- app/src/main/java/ca/aagavin/aaron/CustomerInformation.java
+++ app/src/main/java/ca/aagavin/aaron/CustomerInformation.java
@@ -1,7 +1,10 @@
 package ca.aagavin.aaron;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CustomerInformation extends AppCompatActivity {
 
@@ -10,4 +13,23 @@ public class CustomerInformation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_information);
     }
+
+    public void button_confirm_click(View view){
+        boolean allInputsValid;
+        allInputsValid = this._nameTextValid();
+
+        Toast.makeText(this, "Valid", Toast.LENGTH_SHORT).show();
+
+    }
+
+    private boolean _nameTextValid(){
+        TextView name = (TextView) findViewById(R.id.editText_name);
+        boolean valid = name.getText().toString().length()!=0;
+        if (!valid){
+            name.setError("Name can't be empty");
+        }
+        return valid;
+    }
+
+
 }
